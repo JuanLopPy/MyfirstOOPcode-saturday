c56e4b739be892e9ae18f8efe98b4f3bcc0d77b5
juanm <juanm@JUANMA>
2023-02-20 20:13:18 -0500

    creation class employee

---
+++ MyfirstOOPcode-saturday/MyfirstOOPcode-saturday/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyfirstOOPcode_saturday
+{
+    public abstract class Employee
+    {
+        #region properties
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Date BirthDay{ get; set; }
+        public Date HiringDate { get; set; }
+        public bool Isactive { get; set; }
+
+        #endregion
+        #region methods
+        public abstract int GetValueToPay();
+        public override string ToString() {
+
+            return $"---EMPLOYEE----\n\t " +
+                $"ID: {Id} \n\t " +
+                $"First Name: {FirstName}\n\t " +
+                $"Last Name: {LastName}\n\t " +
+                $"BirthDay: {BirthDay}\n\t " +
+                $"Hiring Date:{HiringDate}\n\t " +
+                $"is active:{Isactive}"; 
+        }
+
+        #endregion
+
+
+
+
+
+
+
+    }
+}
